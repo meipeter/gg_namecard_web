56bfe3b1b352a1369e7b9884aa73a822366cfbe7
meipeter <meipeter114514@outlook.com>
2025-08-22 20:43:49 +0800

    改为unwrap弹窗

--- src/panichook.rs
+++ src/panichook.rs
@@ -58,22 +58,29 @@ cfg_if! {
 
             let html = format!(
                 r#"
-                <div role="alert" class="alert alert-error">
-                  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                  </svg>
-                  <span>{}</span>
+
+                <input type="checkbox" checked="checked" id="my_modal_6" class="modal-toggle 	" />
+                <div class="modal " role="dialog" >
+                  <div class="modal-box bg-error 	text-error-content h-1/2" >
+                    <h3 class="text-lg font-bold">Error!</h3>
+                    <p class="py-4 h-4/5 overflow-scroll"><span>{}</span></p>
+                    <div class="modal-action">
+                      <label for="my_modal_6" class="btn">Close!</label>
+                    </div>
+                  </div>
                 </div>
+
                 "#,
                 msg.replace("\n", "<br>")
             );
             // 设置 div 的内容
             div.set_inner_html(html.as_str());
 
+            //删除所有div
+            // while let Some(child) = document.body().unwrap().first_child() {
+            //         document.body().unwrap().remove_child(&child).expect("Failed to remove child");
+            //     };
             // 将 div 添加到 body 中
-            while let Some(child) = document.body().unwrap().first_child() {
-                    document.body().unwrap().remove_child(&child).expect("Failed to remove child");
-                };
             document.body().unwrap().append_child(&div).unwrap();
             error(msg);
         }
